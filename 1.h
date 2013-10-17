3fc788fb632655b5ee6d983e56dfc3cc23e9fd6b
Houser Kok <hzk11a@acu.edu>
2013-10-17 08:56:59 -0500

    Fixed typo
    Changed name of Constructor/Destructor of MovingBody

--- 1.h
+++ 1.h
@@ -91,8 +91,8 @@ protected:
 
 class MovingBody{ //any object in game that can be associated with a room
 public:
-	MovingObject(){};
-	virtual ~MovingObject(){};
+	MovingBody(){};
+	virtual ~MovingBody(){};
 	virtual int performAction(){}; //return id for action.
 	string name(); //getter
 
@@ -124,3 +124,17 @@ private:
 	string description; //added to description that displays after a room's description	
 	int perishable; // some items may only last x number of turns. -1 for false. may not be in final game
 };
+
+struct attributeList{  //what actions can be done on an object?
+	bool edible; 	//eat command
+	bool takeable;	//take, pickup
+	bool droppable;	// drop, leave, throw
+	bool catchable; //can this object catch on  different object (grappling hook)
+	bool catcher;  	//can this object catch a catchable object (ie a ledge)
+	bool weapon;	//can be used to kill living object, swing command
+	bool alive;		//can die
+	bool destructible;	//can this item be removed entirely
+	int perishible; 	//item destroyed after x turns, 0 for false
+};
+
+//this whole file may become turned into keywords
