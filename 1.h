9b751aa4ff7610cc5e7731b1e912fde7c7d0583f
Houser Kok <hzk11a@acu.edu>
2013-10-16 20:56:23 -0500

    Completed Classes
    Tentative classes with parameters of member functions filled in.
    Currently the system I have in mind is that the checkKeyword function
    would see if the keyword applies to the item, and would call
    performAction function, which would return an int, which is the
    identifier for a certain action, which at this point we think we would
    have to store all of which in an "event handler" type entity.

--- 1.h
+++ 1.h
@@ -1,24 +1,85 @@
+//1st Header File - Existing Stack Implementation.
+#include <cstddef>
+#include <cstring>
+
+template <class T>
+struct Stack {
+   T*  data;	// array of values
+   int size;	// size of array
+   int top;	// index of value currently at top of Stack
+};
+
+template <class T>
+void initialize(Stack<T>&s)
+{
+	s.size = 2;
+	s.data = new T [s.size];
+	s.top = 0;
+}
+
+template <class T>
+void destroy(Stack<T>&s)
+{
+	s.top = 0;
+	delete []s.data;
+	s.data = NULL;
+}
+
+template <class T>
+bool isEmpty(Stack<T> s)
+{
+	return (s.top == 0);
+}
+
+template <class T>
+void push(Stack<T>&s,T x)
+{
+	if (s.top == s.size)
+	{
+		s.size *= 2;
+		T* tmp = new T[s.size];
+		for (int i = 0; i < s.size/2; i++)
+			tmp[i] = s.data[i];
+		delete []s.data;
+		s.data = tmp;
+		tmp = NULL;
+	}
+	s.data[s.top] = x;
+	s.top ++;
+}
+
+template <class T>
+T    pop(Stack<T>&s)
+{
+	s.top --;
+	return s.data[s.top];
+}
+
+//2nd Header File begins here.
 #include <iostream>
 
-//1st Header File begins here.
 class GameInstance{
 private:
-	string name;
-	Stack path;
-	int turnCounter;
-	int points;
-
+	string name; 
+	Stack path; //Stack to keep track of all moves made so far.
+	int turnCounter; //Counter that keeps track of number of moves made.
+	int points; // Counter to keep track of points
 };
 
 class Room{
 public:
-	Room();
+	Room(string n, int numI, Items* list, string* mC, Room* mD);
 	~Room();
-	name(); //getter
-	name(string n); //setter
+	string name(); //getter
+	void name(string n); //setter
+	string shortDescript(); //getter
+	void shortDescript(string newSD); //setter
+	string longDescript(); //getter
+	void longDescript(string newLD); //setter
+	Room* destLookup(string command); //Look up destinaton based on command.
 
 protected:
-	string name;
+	string roomName;
 	int numItems;
 	Items* itemList;
 	string shortDes; //Short description of room.
@@ -30,21 +91,36 @@ protected:
 
 class MovingBody{
 public:
-	MovingObject(string n, bool e, bool p); //name, edible, perishable?
+	MovingObject(){};
 	virtual ~MovingObject(){};
-	virtual performAction(){};
-	name(); //getter
+	virtual int performAction(){}; //return id for action.
+	string name(); //getter
 
 protected:
 	string name;
-	bool edible;
-	bool perishable;
 };
 
 class Player: public MovingBody{
 public:
 	Player(Room* l){location = l;};
 	~Player();
+	string currentLocation(); //getter
+
 private:
+	bool isDead; //Ends game if player is dead. 
+	Item* inventory;
 	Room* location;
+};
+
+class Item: public MovingBody{
+public:
+	Item(string n, bool e, bool p); //Edible? Perishable?
+	~Item();
+	int performAction();
+	void checkKeyword(string s); //calls performAction() if keywords are similar.
+
+private:
+	string* keywords; //array of keywords associated with the object.
+	bool edible;
+	bool perishable;
 };
