e3180a5eea3c3636868774b6f98853b7bfca0331
Houser Kok <hzk11a@acu.edu>
2013-10-23 15:48:57 -0500

    Updated demo.cpp
    Welcome message & instructions.

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include "1.h"
+//#include "1.h"
 
 using namespace std;
 
 int main(){
-	
+	char check1;
+	cout << "Welcome to [WIP] Adventure! Do you need instructions? (Y/N)" << endl;
+	cin >> check1;
+	if (check1 == 'Y' || check1 == 'y')
+	{
+		cout << "You navigate this game purely through text instructions." << endl;
+		cout << "When prompted, please insert what you wish your character to do using commands of not more than two words." << endl;
+		cout << "If stuck, type 'Help' for some hints." << endl;
+	}
 }
