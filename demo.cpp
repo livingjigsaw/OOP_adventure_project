875e2521d5394f4e9305eab97136bff6500fb5c0
BINBINWU <binbinwu@Binbins-MacBook-Pro.local>
2013-11-04 13:18:05 -0600

    room 5-9

--- demo.cpp
+++ demo.cpp
@@ -86,7 +86,7 @@ int main(){
     
     
     //Fourth Room
-    List fourthRList;// third room's item list.
+    List fourthRList;// Fourth room's item list.
     cout << "debug42\n"; //*********current final success**********
     //		initialize(firstRList); //herpaderp the list has no constructor
 	fourthRList.append((Item*)&Hatch);
@@ -99,11 +99,115 @@ int main(){
 	cout << "debug82\n";
 	string* fourthRMC = new string [2]; // second room's move command.
 	Room** fourthRMD =new Room*[2]; fourthRMD[0]=NULL; // second room's move
-    string n4="Thirdroom";
+    string n4="Fourthroom";
     string SD4="through the wooden door, is a pitch black room.";
     string LD4=" There is a machine with the word generator on the wall behind it, as well as a switch in the on position, but it doesn't make connection(the pocketknife can be jammed there to establish connetion, but electrocutes the player if not in off position) so yes, someone can die in the demo TROLLOLOLZ ";
 	Room FourthRoom(n4, 3, &fourthRList ,SD4,LD4, fourthRMC,fourthRMD, 2);
     //Constructing the Fourth room.
+    
+    
+    //Fifth Room
+    List fifthRList;// Fifth room's item list.
+    cout << "debug42\n"; //*********current final success**********
+    //		initialize(firstRList); //herpaderp the list has no constructor
+	fifthRList.append((Item*)&Hatch);
+	cout << "debug52\n";
+	fifthRList.append((Item*)&PocketKnife);
+	cout << "debug62\n";
+	fifthRList.append((Item*)&Clock);
+	cout << "debug72\n";
+	fifthRList.append((Item*)&Painting);
+	cout << "debug82\n";
+	string* fifthRMC = new string [1]; // second room's move command.
+	Room** fifthRMD =new Room*[1]; fifthRMD[0]=NULL; // second room's move
+    string n5="Fifthroom";
+    string SD5="You stand in a bare abandoned laboratory, still clutching the orb. The smell of electricity is in the air as you step down from a raised platform in the middle of the room.";
+    string LD5=" You see a single door to the west (ROOM 6) but realize it is locked electronically, requiring a palm scan to unlock it. (If player pressed button) - You see an open compartment with what looks like a dart gun (DARTGUN) in it. (Not required to finish game).";
+	Room FifthRoom(n5, 3, &fifthRList ,SD5,LD5, fifthRMC,fifthRMD, 1);
+    //Constructing the Fifth room.
+    
+    
+    //Sixth Room
+    List sixthRList;// Fifth room's item list.
+    cout << "debug42\n"; //*********current final success**********
+    //		initialize(firstRList); //herpaderp the list has no constructor
+	sixthRList.append((Item*)&Hatch);
+	cout << "debug52\n";
+	sixthRList.append((Item*)&PocketKnife);
+	cout << "debug62\n";
+	sixthRList.append((Item*)&Clock);
+	cout << "debug72\n";
+	sixthRList.append((Item*)&Painting);
+	cout << "debug82\n";
+	string* sixthRMC = new string [2]; // second room's move command.
+	Room** sixthRMD =new Room*[2]; sixthRMD[0]=NULL; // second room's move
+    string n6="Sixthroom";
+    string SD6="You are in a hallway. There are doors to the east (ROOM 5), north (ROOM 7) and south (Can't enter, door is barricaded). There are signs above each of the doors but two of them are so rusted you can't make out a word.The sign over the south door shows 'STAIRWELL'. ";
+    string LD6=" In what you hope is red paint, you see the words 'GET DOWNSTAIRS IF YOU STILL CAN.' scrawled in large letters across a wall. You hear dripping in the distance.";
+	Room SixthRoom(n6, 3, &sixthRList ,SD6,LD6, sixthRMC,sixthRMD, 2);
+    //Constructing the Sixth room.
+    
+    //Seventh Room
+    List seventhRList;// Seventh room's item list.
+    cout << "debug42\n"; //*********current final success**********
+    //		initialize(firstRList); //herpaderp the list has no constructor
+	seventhRList.append((Item*)&Hatch);
+	cout << "debug52\n";
+	seventhRList.append((Item*)&PocketKnife);
+	cout << "debug62\n";
+	seventhRList.append((Item*)&Clock);
+	cout << "debug72\n";
+	seventhRList.append((Item*)&Painting);
+	cout << "debug82\n";
+	string* seventhRMC = new string [2]; // second room's move command.
+	Room** seventhRMD =new Room*[2]; seventhRMD[0]=NULL; // second room's move
+    string n7="Seventhroom";
+    string SD7="You are in a hallway. There are doors to the east (ROOM 5), north (ROOM 7) and south (Can't enter, door is barricaded). There are signs above each of the doors but two of them are so rusted you can't make out a word.The sign over the south door shows 'STAIRWELL'. ";
+    string LD7=" In what you hope is red paint, you see the words 'GET DOWNSTAIRS IF YOU STILL CAN.' scrawled in large letters across a wall. You hear dripping in the distance.";
+	Room SeventhRoom(n7, 3, &seventhRList ,SD7,LD7, seventhRMC,seventhRMD, 2);
+    //Constructing the Seventh room.
+    
+    //Eighth Room
+    List eighthRList;// Eighth room's item list.
+    cout << "debug42\n"; //*********current final success**********
+    //		initialize(firstRList); //herpaderp the list has no constructor
+	eighthRList.append((Item*)&Hatch);
+	cout << "debug52\n";
+	eighthRList.append((Item*)&PocketKnife);
+	cout << "debug62\n";
+	eighthRList.append((Item*)&Clock);
+	cout << "debug72\n";
+	eighthRList.append((Item*)&Painting);
+	cout << "debug82\n";
+	string* eighthRMC = new string [2]; // second room's move command.
+	Room** eighthRMD =new Room*[2]; eighthRMD[0]=NULL; // second room's move
+    string n8="EighthRoom";
+    string SD8="A door to the west leads back to the cafeteria (ROOM 7) You have found the source of the smell. In the restroom, two unidentifiable dead bodies slump against a wall.";
+    string LD8=" Their bodies are covered with bite marks, and their limbs look as if they were torn off. One of the bodies wears a lab coat, in his hands he cradles a device (DEVICE), his last act seemed to be protecting the device. The other body is even more mangled, but you conclude that he was a janitor based on the ring of keys(JANITORKEYS) he has looped on around his belt.";
+	Room EighthRoom(n8, 3, &eighthRList ,SD8,LD8, eighthRMC,eighthRMD, 1);
+    //Constructing the Eighth room.
+    
+    
+    //Ninth Room
+    List ninthRList;// Ninth room's item list.
+    cout << "debug42\n"; //*********current final success**********
+    //		initialize(firstRList); //herpaderp the list has no constructor
+	ninthRList.append((Item*)&Hatch);
+	cout << "debug52\n";
+	ninthRList.append((Item*)&PocketKnife);
+	cout << "debug62\n";
+	ninthRList.append((Item*)&Clock);
+	cout << "debug72\n";
+	ninthRList.append((Item*)&Painting);
+	cout << "debug82\n";
+	string* ninthRMC = new string [2]; // second room's move command.
+	Room** ninthRMD =new Room*[2]; ninthRMD[0]=NULL; // second room's move
+    string n9="NinthRoom";
+    string SD9="You walk into the room and right on to the edge of a gaping hole. You have found the source of the dripping sounds: A giant vat of what is presumably strong acid lies partially smashed on one end of the room. After an indeterminable time, the acid has corroded into the ground. Acid continues to drip on to the floor below, sizzling as it comes into contact with the tiled floor.";
+    string LD9=" Once your eyes adjusted to the low light, you see a blue glint on the other side of the room; it seems the acid had also destroyed part of a safe. However, you can't reach the safe because of the huge hole in the middle of the room. A door to the south leads back to the cafeteria (ROOM 7). You can climb down the hole to the floor below, but you won't be able to return. (ROOM 10) If the player chooses to USE metallic orb, the blue gem (sapphire) would be attracted to the orb. ";
+	Room NinthRoom(n9, 3, &ninthRList ,SD9,LD9, ninthRMC,ninthRMD, 1);
+    //Constructing the Ninth room.
+
     
     cout<<firstRMD[0]<<endl;
     firstRMC[0]="NORTH";
@@ -119,9 +223,34 @@ int main(){
     
     secondRMC[2]="EAST";
     secondRMD[2]=&FourthRoom;
-    
     fourthRMC[0]="WEST";
     fourthRMD[0]=&SecondRoom;
+    
+    fourthRMC[1]="Teleportation";
+    fourthRMD[1]=&FifthRoom;
+    
+    fifthRMC[0]="WEST";
+    fifthRMD[0]=&SixthRoom;
+    
+    sixthRMC[0]="EAST";
+    sixthRMD[0]=&FifthRoom;
+    sixthRMC[0]="NORTH";
+    sixthRMD[0]=&SeventhRoom;
+    
+    seventhRMC[0]="SOUTH";
+    seventhRMD[0]=&SixthRoom;
+    seventhRMC[1]="EAST";
+    seventhRMD[1]=&EighthRoom;
+    seventhRMC[2]="NORTH";
+    seventhRMD[2]=&NinthRoom;
+    
+    eighthRMC[0]="WEST";
+    eighthRMD[0]=&SeventhRoom;
+    
+    ninthRMC[0]="SOUTH";
+    ninthRMD[0]=&SeventhRoom;
+    
+    
 
 	
 	Player Player1(&FirstRoom);
